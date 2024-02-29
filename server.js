9421218351f5024896729531b20f6c92b4d99cd2
TianNyAvo <t.andriatoavina@yahoo.com>
2024-02-29 15:33:36 +0300

    remove unused middlewares

--- server.js
+++ server.js
@@ -20,12 +20,6 @@ console.log('creating views if not exists');
 databaseServices.createAppointmentView();
 databaseServices.createempAvgView();
 
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static('public'));
-
 app.use(cors());
 
 app.listen(3000, function () {
